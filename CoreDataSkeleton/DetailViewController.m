23cf7d1527f94d154a2df4cf7e2ae3dbc496b945
luojun <jun@dragonflystudios.ca>
2014-07-04 21:08:35 -0400

    functionality for adding GitHub user name

--- CoreDataSkeleton/DetailViewController.m
+++ CoreDataSkeleton/DetailViewController.m
@@ -34,7 +34,7 @@
 - (void)configureView {
     // Update the user interface for the detail item.
     if (self.detailItem) {
-        self.detailDescriptionLabel.text = [[self.detailItem valueForKey:@"timeStamp"] description];
+        self.detailDescriptionLabel.text = [[self.detailItem valueForKey:@"userName"] description];
     }
 }
 
